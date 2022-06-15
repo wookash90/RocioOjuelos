22113d4d1c3cc30c104fc505b7ac2f86841fec07
Lukas <84273399+wookash90@users.noreply.github.com>
2022-06-15 13:39:16 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Toggle Menu
 const openMenu = document.getElementById('menu');
 const closeMenu = document.getElementById('closeMenu');
-let overlay = document.getElementById('overlay');
+const overlay = document.getElementById('overlay');
+
 
 overlay.style.display = "none";
 
@@ -15,4 +16,59 @@ function menuToggle() {
 }
 
 openMenu.addEventListener('click', menuToggle);
-closeMenu.addEventListener('click', menuToggle)
+closeMenu.addEventListener('click', menuToggle);
+
+
+// Checkbox
+const checkbox = document.getElementById('checkbox');
+
+function tickCheckbox() {
+    if(checkbox.style.backgroundColor == ""){
+        checkbox.style.backgroundColor = "#000";
+    } else {
+        checkbox.style.backgroundColor = ""
+    }
+}
+
+// Read More
+const dots = document.getElementById("span_dots");
+const moreInfo = document.getElementById("span_more");
+const btnMore = document.getElementById("read_more_about_btn");
+const masOcultar = document.getElementById("masOcultar");
+
+function readMoreInfo() {
+    if(dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnMore.innerHTML = "+ leer más";
+        moreInfo.style.display = "none";
+        btnMore.style.cursor = "pointer";
+    } else {
+        dots.style.display = "none";
+        btnMore.innerHTML = "- ocultar";
+        moreInfo.style.display = "inline";
+        btnMore.style.cursor = "pointer";
+    }
+}
+
+const dots2 = document.getElementById("span_dots2");
+const moreInfo2 = document.getElementById("span_more2");
+const btnMore2 = document.getElementById("read_more_about_btn2");
+const masOcultar2 = document.getElementById("masOcultar2");
+
+function readMoreInfo2() {
+    if(dots2.style.display === "none") {
+        dots2.style.display = "inline";
+        btnMore2.innerHTML = "+ leer más";
+        moreInfo2.style.display = "none";
+        btnMore2.style.cursor = "pointer";
+    } else {
+        dots2.style.display = "none";
+        btnMore2.innerHTML = "- ocultar";
+        moreInfo2.style.display = "inline";
+        btnMore2.style.cursor = "pointer";
+    }
+}
+
+
+
+
